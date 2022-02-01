cf6a4f44c95eac5196f795ac1ef55339b7916589
xzrie <muhammadfajri.gs@gmail.com>
2022-02-01 15:28:38 +0700

    add Card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Card from './components/Card';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="max-w-screen-sm h-screen my-0 mx-auto bg-slate-50">
+      <div className="flex flex-row justify-evenly">
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default App;
