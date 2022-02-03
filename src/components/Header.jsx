4073451d4dae43af01bd11b809447dd66d3fc076
xzrie <muhammadfajri.gs@gmail.com>
2022-02-03 16:11:22 +0700

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+const Header = ({ title, subTitle, rightBtn }) => {
+  return (
+    <header className="h-18 sticky top-0 mb-1 bg-slate-50">
+      <div className="h-16 flex flex-row items-center px-3 py-2">
+        {/* // * Back Button */}
+        <div className="basis-2/8 text-center mx-3">
+          <a href="/" className="text-slate-900">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
+          </a>
+        </div>
+        {/* //* Header Title */}
+        <div className="basis-5/8 grow text-slate-900 px-2">
+          <h1 className="text-xl font-extrabold">{title}</h1>
+          {!null && <h4 className="text-sm font-semibold">{subTitle}</h4>}
+        </div>
+        {/* //* Right Button [option: add, more, null] */}
+        <div className="basis-1/8 text-center mx-3">
+          {rightBtn === 'add' ? (
+            <button className="text-slate-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>
+            </button>
+          ) : null || rightBtn === 'more' ? (
+            <button className="text-slate-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                />
+              </svg>
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
