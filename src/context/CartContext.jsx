76335d8d21073e591d63b80d4cae2b3b203b40ab
xzrie <muhammadfajri.gs@gmail.com>
2022-04-05 16:37:26 +0700

    Refactoring cart context | Adding new reducer method for clear cart state

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useEffect, useReducer, useContext } from 'react';
+import { cartReducer, initializer } from './Reducers';
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  // * Check & Get cart from localstorage
+  const [cart, dispatch] = useReducer(cartReducer, [], initializer);
+
+  // * Save cart state to local storage
+  useEffect(() => {
+    console.log('Item(s) added to cart', cart);
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  return (
+    <CartContext.Provider value={{ cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const CartState = () => useContext(CartContext);
+
+export default CartProvider;
--- src/context/Reducers.jsx
+++ src/context/Reducers.jsx
@@ -1,43 +1,36 @@
+const initialState = [];
+
+export const initializer = (initialValue = initialState) =>
+  JSON.parse(localStorage.getItem('cart')) || initialValue;
+
 export const cartReducer = (state, action) => {
   switch (action.type) {
-    case 'FETCH_SUCCESS':
-      return {
-        ...state,
-        data: action.payload,
-        loading: false,
-        error: null,
-      };
-    case 'FETCH_ERROR':
-      return {
-        ...state,
-        data: null,
-        loading: false,
-        error: action.error,
-      };
     case 'ADD_TO_CART':
-      return {
+      return [
         ...state,
-        cart: [...state.cart, { ...action.payload, product_id: action.payload.id, amount: 1, customer_notes: '' }],
-      };
+        {
+          ...action.payload,
+          product_id: action.payload.id,
+          amount: 1,
+          customer_notes: '',
+        },
+      ];
     case 'REMOVE_FROM_CART':
-      return {
-        ...state,
-        cart: state.cart.filter(c => c.id !== action.payload.id),
-      };
+      return state.filter(c => c.id !== action.payload.id);
     case 'CHANGE_CART_QTY':
-      return {
-        ...state,
-        cart: state.cart.filter(c =>
-          c.id === action.payload.id ? (c.amount = action.payload.amount) : c.amount
-        ),
-      };
+      return state.filter(c =>
+        c.id === action.payload.id
+          ? (c.amount = action.payload.amount)
+          : c.amount
+      );
     case 'CHANGE_CART_NOTE':
-      return {
-        ...state,
-        cart: state.cart.map(c =>
-          c.id === action.payload.id ? { ...c, customer_notes: action.payload.customer_notes } : c
-        ),
-      };
+      return state.map(c =>
+        c.id === action.payload.id
+          ? { ...c, customer_notes: action.payload.customer_notes }
+          : c
+      );
+    case 'CLEAR_CART':
+      return initialState;
     default:
       return state;
   }
